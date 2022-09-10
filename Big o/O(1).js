d9723dcf5cfdc397b2e05316e0c8f66b433fa392
akhileshgowda <akhileshgowda7@gmail.com>
2022-09-10 13:12:24 -0400

    Big O

---
+++ Big o/O(1).js
@@ -0,0 +1,9 @@
+//Does not matter how long the input is it runs in constant time
+const boxes = [0,1,2,3,4,5];
+
+
+function logFirstBoxes(boxes){
+    console.log(boxes[1])
+}
+
+logFirstBoxes(boxes);
