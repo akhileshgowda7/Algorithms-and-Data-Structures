a7febe14e9d85303303d43f2261d3c7e44edd23c
akhileshgowda <akhileshgowda7@gmail.com>
2022-09-17 17:05:45 -0400

    Added remove method

--- OOPS/linkedList.js
+++ OOPS/linkedList.js
@@ -34,7 +34,7 @@ class LinkedList{
         return this;
     }
     insert(index,value){
-        if(index>=this.insert){
+        if(index>=this.length){
            return this.append(value)
         }
         let newNode = new Node(value);
@@ -45,6 +45,26 @@ class LinkedList{
         newNode.next = holdingPointer;
         this.length++;
         return this;
+    }
+    remove(index){
+        if(index==this.length){
+           const newTail =  this.traverseToIndex(index-1)
+           this.tail = newTail
+           this.length--
+           return this 
+        }
+        if(index == 0){
+            this.head = this.head.next;
+            this.length--
+           return this 
+        }
+
+        const leader = this.traverseToIndex(index-1);
+        const unwantedNode = leader.next
+         leader.next = unwantedNode.next
+         this.length-- 
+         return this;
+
     }
         traverseToIndex(index){
          let tempIndex = 0;
@@ -71,8 +91,10 @@ const myLinkedList = new LinkedList(10)
 myLinkedList.append(5)
 myLinkedList.append(2)
 myLinkedList.prepend(4)
-myLinkedList.append(10);
+myLinkedList.append(14);
 myLinkedList.append(11);
 myLinkedList.insert(3,13)
+console.log(myLinkedList.printList())
+myLinkedList.remove(6)
 // myLinkedList.printList()
 console.log(myLinkedList.printList())
